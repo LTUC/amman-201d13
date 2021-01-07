640ef9641a20b71fef9ada58137e7a57e7535da1
amani-bn <amanialabdallah@yahoo.com>
2021-01-07 18:45:29 +0200

    add branch

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -104,12 +104,18 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+// var testArray1=[2,3,4];
 function multiplyArray(multArr) { //eslint-disable-line
+  var multtot=1;
+  for (var i=0;i<multArr.length;i++){
+  multtot=multiply(multtot,multArr[i])[0]
+}
+return [multtot,'The numbers '+multArr+' have a product of '+multtot+'.'];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+ testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
