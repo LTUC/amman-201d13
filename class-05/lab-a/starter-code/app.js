eeb496f4f2554a8c4066661cc38f2c7bd33d1457
anasswalha95 <anas.swalha@yahoo.com>
2021-01-07 15:55:24 +0200

    Fourth challenge is Done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -82,11 +82,15 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  var summation = sum(sumArr[0], sum(sumArr[1], sumArr[2])[0])[0];
+  var text = sumArr[0] + "," + sumArr[1] + "," + sumArr[2] + " was passed in as an array of numbers, and " + summation + " is their sum.";
+  return [summation, text];
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
