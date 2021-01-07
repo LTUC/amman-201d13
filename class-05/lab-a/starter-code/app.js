c9e81825b19b0c31cbb45928d4e88c9a1fc447c6
anasswalha95 <anas.swalha@yahoo.com>
2021-01-07 15:19:13 +0200

    Third Challange is Done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -54,11 +54,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var summation = a + b + c;
+  var Product = a * b * c;
+  var text1 = a + " and " + b + " and " + c + " sum to " + summation + ".";
+  var text2 = "The product of " + a + " and " + b + " and " + c + " is " + Product + "."
+  return [summation, Product, text1, text2]
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
