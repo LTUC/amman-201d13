713f2316ee04c7a289b2e537d3af0e4892ae7c71
anasswalha95 <anas.swalha@yahoo.com>
2021-01-07 16:18:45 +0200

    Sixth Challenge is Done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -138,10 +138,34 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  var product = 1;
+  var text = "The numbers "
+
+  for (var i = 0; i < dynamicArray.length; i++) {
+
+
+    product *= dynamicArray[i];
+
+
+    if ((i + 1) != dynamicArray.length) {
+      text += dynamicArray[i] + ","
+    }
+
+    else {
+      text += dynamicArray[i]
+    }
+
+  }
+  text += " have a product of " + product + ".";
+
+
+
+  return [product, text]
+
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
