37ad6e707145f88d4ed2f79052646138be64596b
ASEEL ALBANNA <ASEELNIHAD.AN@GMAIL.COM>
2021-01-07 15:17:06 +0200

    Problem 2 is solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -1,5 +1,5 @@
 'use strict';
-/////////////////////////////////////
+////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  var mult = a * b;
+  var reultString = 'The product of '+ a + ' and ' + b +' is ' + mult +'.';
 
+  return[mult,reultString];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
