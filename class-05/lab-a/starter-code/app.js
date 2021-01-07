59a7c4411ad74b08aebd4ce8bcfde4605c9537d8
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-01-07 14:55:35 +0200

    p1

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -31,7 +31,7 @@ function multiply(a, b) {
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -49,10 +49,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
 	//eslint-disable-line
+	// return [
+	// 	a + b + c,
+	// 	a * b * c,
+	// 	`${a} and ${b} and ${c} sum to ${a + b + c}.`,
+	// 	`The product of ${a} and ${b} and ${c} is ${a * b * c}.`,
+	// ];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -71,8 +77,8 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) {
 	//eslint-disable-line
+	
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
 // testSumArray(testArray);
@@ -92,6 +98,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) {
 	//eslint-disable-line
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
@@ -119,7 +126,8 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) {
-	//eslint-disable-line
+    //eslint-disable-line
+   
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
