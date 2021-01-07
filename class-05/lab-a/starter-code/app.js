e4599d077694c9023dd7fa3e29645f64d2066f44
mzool <mohammad_alzoul97@yahoo.com>
2021-01-07 22:42:51 +0200

    problem 5

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -112,12 +112,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+var mulArr = [2,3,4];
 function multiplyArray(multArr) { //eslint-disable-line
-
+multiply1(mulArr[0],mulArr[1],mulArr[2]);
+var returnMul = 'The numbers '+ mulArr[0]+','+mulArr[1]+','+mulArr[2]+' have a product of 24.'
+return[multiply3,returnMul];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
