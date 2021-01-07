d76f2fd92a0e65d9871eb8cee7a34c0ce5e2511c
lina-yousef <lina.aa96@hotmail.com>
2021-01-07 20:23:21 +0200

    Q3 proplem

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -87,11 +87,15 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+  var sumArr  = sum(sum(sumArr[0], sumArr[1])[0], sumArr[2])[0];
+  
+  var arrString = testArray[0]+','+ testArray[1]+','+ testArray[2]+ ' was passed in as an array of numbers,' + ' and ' + sumArr + ' is their sum.';
+  return [sumArr , arrString]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
