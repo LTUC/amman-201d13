639ac72487ac20a2349bcad91faa9a7a9178bfb9
aya-alasmar <aya.asmar32@gmail.com>
2021-01-07 14:41:40 +0200

    add new brach problem2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,15 +8,15 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+ function sum(a, b) { //eslint-disable-line
   var sum = a+b;
   var reultString = 'The sum of '+a+' and '+b +' is '+sum+'.';
   return[sum,reultString];
 
-}
+} 
 
 // Here is the test for sum(); uncomment it to run it
- testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -29,12 +29,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function tesMultiply(a, b) { //eslint-disable-line
+  var multiplay=a*b;
+  var multiplaySting= "The product of "+a+" and "+b+" is "+multiplay +" .";
+  return[multiplay,multiplaySting];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+console.log(tesMultiply(5,9));
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
