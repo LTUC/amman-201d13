e8bba38822c8d222eb54e2cfaf47b41817bafd81
sarahoth <sarahanwar234@gmail.com>
2021-01-07 17:49:55 +0200

    addin prpblem 4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -86,12 +86,21 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+ 
+  var a = sumArr[0];
+  var b = sumArr[1];
+  var c = sumArr[2];
+  var sum4= sumAndMultiply(a,b,c);
+  //console.log(sum4[0]);
+  
+  var resalt = a+","+b+","+c +" was passed in as an array of numbers, and "+ sum4[0] +" is their sum.";
+   //console.log(resalt);
+  return [sum4[0],resalt];
 
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
