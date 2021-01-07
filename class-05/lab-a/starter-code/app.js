d019b8aae235a76b5859ef788421a44257f3cdee
Fatensamman <arch.fatensamman@gmail.com>
2021-01-07 17:30:16 +0200

    make-test

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -70,7 +70,7 @@ return[sum2,multiply2,string1,string2];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//  testSumAndMultiply(4,7,5);
+  testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
