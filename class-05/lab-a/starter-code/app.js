18a38a1a34557a2e9a5145f9597c26e7a34182e8
Fatensamman <arch.fatensamman@gmail.com>
2021-01-07 21:52:32 +0200

    edit-problem4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -93,9 +93,10 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  var sum3 = (sumAndMultiply(sumArr[0],sumArr[1],sumArr[2])[0])
-  var string = sumArr+' was passed in as an array of numbers, and '+sum3+' is their sum.'
-return[sum3,string]
+  var plus1= sum(sumArr[0],sumArr[1]);
+  var plus2= sum(plus1[0],sumArr[2]);
+  var string= sumArr+' was passed in as an array of numbers, and '+plus2[0]+' is their sum.';
+  return[plus2[0],string];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
