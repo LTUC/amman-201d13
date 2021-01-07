9ae574f8d1caa02fd0cc30f2ddc33be9b0488145
shatha9522 <shatha_abuali95@hotmail.com>
2021-01-07 17:29:06 +0200

    prob3

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -54,11 +54,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  var sum = a+b+c;
+  var multiplay =  a * b * c;
+  var res3 = a+' and '+b+' and '+c+' sum to '+sum+'.';
+  var res4 = 'The product of '+a+' and '+b+' and '+c+' is '+multiplay+'.';
+  return[sum,multiplay,res3,res4];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
