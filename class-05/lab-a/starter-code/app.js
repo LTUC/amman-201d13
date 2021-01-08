e4f4d121d0018be8bf6fa64daae4f17594b1e70c
ali mohammad <des.alialjeatawy@gmail.com>
2021-01-08 22:42:53 +0200

    prob4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -78,15 +78,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
-
 function sumArray(sumArr) { //eslint-disable-line
- 
+
+  var aliarray=0;
+
+  for (var i =0; i<sumArr.length; i++){
+    aliarray=sum(aliarray,sumArr[i])[0];
+  }
+  var y = ''+2+','+3+','+4+' was passed in as an array of numbers, and ' +aliarray+' is their sum.';
+  
+  return [aliarray,y];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -101,8 +108,10 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function multiplyArray(multArr) { //eslint-disable-line
 
+function multiplyArray(multArr) { //eslint-disable-line
+  
+ 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
