fb852bd08aaed4e190d1fa517ddf37c2c936aed5
Fatensamman <arch.fatensamman@gmail.com>
2021-01-09 23:37:45 +0200

    edit-problem4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -122,10 +122,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-var product1= multiply(multArr[0],multArr[1]);
-var product2=multiply(product1[0],multArr[2]);
-var string= 'The numbers '+multArr+' have a product of '+product2[0];
-return[product2,string];
+
 
 }
 // var j =0
